3e15f9fa26ac3ef27aa4e00565c27b72d122fb39
andibabett <andi_babett@yahoo.com>
2019-10-26 19:06:39 +0300

    item database configuration

--- src/main/java/org/fasttrackit/phonebook/domain/phonebookItem.java
+++ src/main/java/org/fasttrackit/phonebook/domain/phonebookItem.java
@@ -1,4 +1,66 @@
 package org.fasttrackit.phonebook.domain;
 
 public class phonebookItem {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String number;
+    private boolean done;
+
+
+
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "phonebookItem{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", number='" + number + '\'' +
+                ", done=" + done +
+                '}';
+    }
 }
---
+++ src/main/java/org/fasttrackit/phonebook/persistance/DatabaseConfiguration.java
@@ -0,0 +1,7 @@
+package org.fasttrackit.phonebook.persistance;
+
+public class DatabaseConfiguration {
+
+
+
+}
